1bfe2215b8e037f205fd840720974fc95354be22
Rajabov Bobokhon <rboboxon6055@gmail.com>
2020-08-26 23:35:55 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const btnQuote = document.getElementById('btnQuote');
+const outputText = document.getElementById('outputText');
+
+let quotes =  [  ["Stay Hungry. Stay Foolish.", "Steve Jobs"],
+["Good Artists Copy, Great Artists Steal.", "Pablo Picasso"],
+["Argue with idiots, and you become an idiot.", "Paul Graham"],
+["Be yourself; everyone else is already taken.", "Oscar Wilde"],
+["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]];
+
+btnQuote.addEventListener('click', function(){
+    var randomQuotes = quotes[Math.floor(Math.random()*quotes.length)]
+    outputText.innerHTML=randomQuotes;
+})
+
+
+        
